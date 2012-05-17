4a50f0cd9432de6aecc4cd39d16172b7f61601c0
Guilherme Araujo <gui2811@gmail.com>
2012-05-17 14:38:15 -0300

    Agora com python xD

---
+++ workfeeds.py
@@ -0,0 +1,54 @@
+import os
+import webapp2
+import jinja2
+import urllib2
+import json
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+			       autoescape = True)
+
+class Tweet():
+	def __init__(self, created_at,from_user_name,profile_img,text,user):
+		self.created_at = created_at
+		self.from_user_name = from_user_name	
+		self.profile_img = profile_img
+		self.text = text
+		self.profile_link = 'https://twitter.com/#!/%s' % user
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+	    self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+	    t = jinja_env.get_template(template)
+	    return t.render(params)
+
+	def render(self, template, **kw):
+	    self.write(self.render_str(template,**kw))
+
+	def retrieve_newers(filtro = None):
+	    p = urllib2.urlopen('http://search.twitter.com/search.json?q=%40twitterapi')
+	    c = p.read()
+	    j = json.loads(c)
+	    tweets = []
+	    for c in j['results']:
+		created_at = c['created_at']
+		from_user_name = c['from_user_name']
+		profile_img = c['profile_image_url']
+		text = c['text']
+		user = c['from_user']
+		tweet = Tweet(created_at,from_user_name,profile_img,text,user)
+		print tweet.created_at
+		tweets.append(tweet)
+	    return tweets
+
+class MainPage(Handler):
+	def write_form(self, newers):
+	    self.render("front_template.html", tweets = newers)
+
+	def get(self):
+	    tweets = self.retrieve_newers()
+	    self.write_form(tweets)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug = True)
