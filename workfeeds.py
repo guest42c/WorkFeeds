434e3e6526e7a8b4146a99c07c1adf2949d2fcc0
Guilherme Araujo <gui2811@gmail.com>
2012-05-20 20:49:15 -0300

    Um filtro funcional mas que precisa ser melhorado, links nos twits para urls e usuarios (precisa ser melhorado, bugs)

--- workfeeds.py
+++ workfeeds.py
@@ -5,10 +5,16 @@ import urllib2
 import json
 import urlparse
 import re
+import logging
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 			       autoescape = True)
+
+logger = logging.getLogger('tweetvagas_application')
+logger.setLevel(logging.DEBUG)
+
+MAX_TWEETS = 3
 
 class Tweet():
 	def __init__(self, created_at,from_user_name,profile_img,text,user):
@@ -19,8 +25,12 @@ class Tweet():
 		self.profile_link = 'https://twitter.com/#!/%s' % user
 
 def find_urls(atext):
-    url_list = re.findall('(http://[^"\' ]+)',atext)
+    url_list = re.findall('(http://[^"\' ]+|https://[^"\' ]+)',atext)
     return url_list
+
+def find_users(atext):
+	users_list = re.findall('@([A-Za-z0-9_]+)',atext)
+	return users_list
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -35,13 +45,17 @@ class Handler(webapp2.RequestHandler):
 
 	def retrieve_newers(self, t_query = None):
 		try:		
-			url = 'http://search.twitter.com/search.json?q=@TweetVagas'
-			for condition in t_query:
-				url = url + "&" + condition
+			url = 'http://search.twitter.com/search.json?q=from:mettaonline'
+			if t_query:
+				for item in t_query:
+					url = url + "%20AND%20" + str(item)
+					logger.info(item)
+					logger.info(url)
 			p = urllib2.urlopen(url)
 			c = p.read()
 			j = json.loads(c)
 			tweets = []
+			i = 0
 			for c in j['results']:
 				created_at = c['created_at']
 				from_user_name = c['from_user_name']
@@ -49,10 +63,17 @@ class Handler(webapp2.RequestHandler):
 				text = c['text']
 				urls = find_urls(text)
 				for u in urls:
-					text = text.replace(u,'<a href="%s" target="_blank">%s</a>' % (u,u))
+					text = text.replace(u,'<a href="%s" target="_blank">%s</a> ' % (u,u))
+				users_list = find_users(text)
+				for u in users_list:
+					text = text.replace(u+" ",'<a href="https://twitter.com/#!/%s" target="_blank">%s </a>' % (u[0:],u))
 				user = c['from_user']
 				tweet = Tweet(created_at,from_user_name,profile_img,text,user)
 				tweets.append(tweet)
+				if i<MAX_TWEETS:
+					i = i + 1
+				else:
+					break
 		except:
 			tweets = []
 		return tweets
@@ -67,7 +88,10 @@ class MainPage(Handler):
 
 	def post(self):
 	    filtro = self.request.get('filtro')
-	    tweets = self.retrieve_newers(filtro.split(" "))
+	    if filtro:
+	    	tweets = self.retrieve_newers(filtro.split(" "))
+	    else:
+	    	tweets = self.retrieve_newers()
 	    self.write_form(tweets,filtro)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug = True)
